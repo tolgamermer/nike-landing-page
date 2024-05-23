93dd9693d39b847053c59d55ba44432765204c11
Tolga Recep Mermer <tolgarecepmermer@gmail.com>
2024-05-23 20:03:04 +0300

    initial commit

---
+++ src/sections/index.js
@@ -0,0 +1,20 @@
+import Hero from "./Hero";
+import PopularProducts from "./PopularProducts";
+import CustomerReviews from "./CustomerReviews";
+import Services from "./Services"
+import Subscribe from "./Subscribe";
+import SuperQuality from "./SuperQuality";
+import SpecialOffer from "./SpecialOffer";
+import Footer from "./Footer";
+
+
+export {
+    Hero,
+    PopularProducts,
+    SuperQuality,
+    CustomerReviews,
+    Services,
+    Subscribe,
+    SpecialOffer,
+    Footer
+}
